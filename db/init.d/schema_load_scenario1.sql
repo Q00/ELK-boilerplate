ee57a251dcaae0c7fd95c58dd1f411f99b490673
itm12 <jqyu.lee@gmail.com>
2021-09-06 01:20:18 +0900

    add init sql

---
+++ db/init.d/schema_load_scenario1.sql
@@ -0,0 +1,2 @@
+LOAD DATA INFILE './scenario_1/KSG_KSG2019111100AMUS01a_20210101.csv' INTO TABLE water.scenario1 FIELDS TERMINATED BY ',' (`toc_no`, `cnsmr_no`, `mesure_de`, `mesure_tm`, `legal_dong_cd`, `sg_pwrer_use_am`, `all_pwrer_usm_am`);
+
